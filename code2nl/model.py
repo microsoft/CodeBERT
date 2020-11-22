3aafd05ed0a6d0fb4235b6e566eb68fee5a782ac
guody5 <824854534@qq.com>
2020-11-23 00:03:08 +0800

    Update model.py

--- code2nl/model.py
+++ code2nl/model.py
@@ -162,7 +162,7 @@ class Beam(object):
 
         # bestScoresId is flattened beam x word array, so calculate which
         # word and beam each score came from
-        prevK = bestScoresId / numWords
+        prevK = bestScoresId // numWords
         self.prevKs.append(prevK)
         self.nextYs.append((bestScoresId - prevK * numWords))
 
